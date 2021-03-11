e900bfe5e8feac1823d18f91d20e1bd0ef01c1b5
Jonas Sander <jonas.sander@t-online.de>
2021-03-11 11:54:15 +0100

    Fix sphinx error by setting the master document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,9 @@ release = 'alpha'
 
 # -- General configuration ---------------------------------------------------
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
