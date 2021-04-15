a3a6d91e9524709fe92a97800cdf8753bca4ffa0
Christopher Peredy <16451370+mastercaution@users.noreply.github.com>
2021-04-15 14:47:48 +0200

    Make attack runner save attack objects

--- pepr/attack_runner.py
+++ pepr/attack_runner.py
@@ -4,6 +4,7 @@ Attack runner for convenient pentesting with multiple configurations.
 
 import os
 import logging
+import pickle
 
 import numpy as np
 import tensorflow as tf
@@ -16,7 +17,8 @@ from pepr.privacy import mia, gmia
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def run_attacks(yaml_path, save_path, pdf, functions):
+
+def run_attacks(yaml_path, report_save_path, attack_obj_save_path, pdf, functions):
     """
     Run multtiple attacks configured in a YAML file.
 
@@ -24,35 +26,66 @@ def run_attacks(yaml_path, save_path, pdf, functions):
     ----------
     yaml_path : str
         Path to attack configuration file in YAML format.
-    save_path : str
+    report_save_path : str
         Path where to save the report files.
+    attack_obj_save_path : str
+        Path where to save the serialized attack objects.
     pdf : bool
         If true, the attack runner will build the LaTex report and save an PDF to the
         save path.
     functions : dict
         Dictionary containing the functions which return TensorFlow models. The keys
         should correspond to the strings in the configuration file.
+
+    Returns
+    -------
+    dict
+        Dictionary with attack alias and the corossponding path to the serialized attack
+        object.
     """
+
+    attack_object_paths = {}
+
+    # Pickle helper
+    def pickle_attack_obj(attack_obj):
+        with open(
+            f"{attack_obj_save_path}/{attack_obj.attack_alias}.pickle", "wb"
+        ) as f:
+            attack_obj.obj_save_path = attack_obj_save_path
+            pickle.dump(attack_obj, f)
+            attack_object_paths[
+                f"{attack_obj.attack_alias}"
+            ] = f"{attack_obj_save_path}/{attack_obj.attack_alias}.pickle"
+
     # Parse YAML
     with open(yaml_path) as f_stream:
         data = yaml.load(f_stream)
+        attack_obj_save_path
 
-        os.makedirs(save_path + "/fig", exist_ok=True)
+        os.makedirs(attack_obj_save_path, exist_ok=True)
+        os.makedirs(report_save_path + "/fig", exist_ok=True)
         sections = []
 
-        attack_objects = []
         for i, yaml_attack_pars in enumerate(data["attack_pars"]):
             attack_type = yaml_attack_pars["attack_type"]
             if attack_type == "mia":
                 # Parse MIA configuration
-                shadow_model_function = functions[yaml_attack_pars["create_compile_shadow_model"]]
-                attack_model_function = functions[yaml_attack_pars["create_compile_attack_model"]]
+                shadow_model_function = functions[
+                    yaml_attack_pars["create_compile_shadow_model"]
+                ]
+                attack_model_function = functions[
+                    yaml_attack_pars["create_compile_attack_model"]
+                ]
 
                 yaml_data_conf = data["data_conf"][i]
                 shadow_indices = list(np.load(yaml_data_conf["shadow_indices_path"]))
                 target_indices = list(np.load(yaml_data_conf["target_indices_path"]))
-                evaluation_indices = list(np.load(yaml_data_conf["evaluation_indices_path"]))
-                record_indices_per_target = np.load(yaml_data_conf["record_indices_per_target_path"])
+                evaluation_indices = list(
+                    np.load(yaml_data_conf["evaluation_indices_path"])
+                )
+                record_indices_per_target = np.load(
+                    yaml_data_conf["record_indices_per_target_path"]
+                )
 
                 target_models = []
                 for path in yaml_attack_pars["target_model_paths"]:
@@ -62,7 +95,9 @@ def run_attacks(yaml_path, save_path, pdf, functions):
                 attack_pars = {
                     "number_classes": yaml_attack_pars["number_classes"],
                     "number_shadow_models": yaml_attack_pars["number_shadow_models"],
-                    "shadow_training_set_size": yaml_attack_pars["shadow_training_set_size"],
+                    "shadow_training_set_size": yaml_attack_pars[
+                        "shadow_training_set_size"
+                    ],
                     "create_compile_shadow_model": shadow_model_function,
                     "create_compile_attack_model": attack_model_function,
                     "shadow_epochs": yaml_attack_pars["shadow_epochs"],
@@ -72,10 +107,10 @@ def run_attacks(yaml_path, save_path, pdf, functions):
                 }
 
                 data_conf = {
-                    'shadow_indices': shadow_indices,
-                    'target_indices': target_indices,
-                    'evaluation_indices': evaluation_indices,
-                    'record_indices_per_target': record_indices_per_target,
+                    "shadow_indices": shadow_indices,
+                    "target_indices": target_indices,
+                    "evaluation_indices": evaluation_indices,
+                    "record_indices_per_target": record_indices_per_target,
                 }
 
                 # Create attack object
@@ -89,18 +124,26 @@ def run_attacks(yaml_path, save_path, pdf, functions):
                 )
 
                 attack.run()
-                attack.create_attack_section(save_path)
+                attack.create_attack_section(report_save_path)
                 sections.append(attack.report_section)
+
+                pickle_attack_obj(attack)
 
             elif attack_type == "gmia":
                 # Parse MIA configuration
                 model_function = functions[yaml_attack_pars["create_compile_model"]]
 
                 yaml_data_conf = data["data_conf"][i]
-                reference_indices = list(np.load(yaml_data_conf["reference_indices_path"]))
+                reference_indices = list(
+                    np.load(yaml_data_conf["reference_indices_path"])
+                )
                 target_indices = list(np.load(yaml_data_conf["target_indices_path"]))
-                evaluation_indices = list(np.load(yaml_data_conf["evaluation_indices_path"]))
-                record_indices_per_target = np.load(yaml_data_conf["record_indices_per_target_path"])
+                evaluation_indices = list(
+                    np.load(yaml_data_conf["evaluation_indices_path"])
+                )
+                record_indices_per_target = np.load(
+                    yaml_data_conf["record_indices_per_target_path"]
+                )
 
                 target_models = []
                 for path in yaml_attack_pars["target_model_paths"]:
@@ -109,8 +152,12 @@ def run_attacks(yaml_path, save_path, pdf, functions):
                 # Setup dictionaries for attack
                 attack_pars = {
                     "number_classes": yaml_attack_pars["number_classes"],
-                    "number_reference_models": yaml_attack_pars["number_reference_models"],
-                    "reference_training_set_size": yaml_attack_pars["reference_training_set_size"],
+                    "number_reference_models": yaml_attack_pars[
+                        "number_reference_models"
+                    ],
+                    "reference_training_set_size": yaml_attack_pars[
+                        "reference_training_set_size"
+                    ],
                     "create_compile_model": model_function,
                     "reference_epochs": yaml_attack_pars["reference_epochs"],
                     "reference_batch_size": yaml_attack_pars["reference_batch_size"],
@@ -120,10 +167,10 @@ def run_attacks(yaml_path, save_path, pdf, functions):
                 }
 
                 data_conf = {
-                    'reference_indices': reference_indices,
-                    'target_indices': target_indices,
-                    'evaluation_indices': evaluation_indices,
-                    'record_indices_per_target': record_indices_per_target,
+                    "reference_indices": reference_indices,
+                    "target_indices": target_indices,
+                    "evaluation_indices": evaluation_indices,
+                    "record_indices_per_target": record_indices_per_target,
                 }
 
                 # Create attack object
@@ -137,7 +184,11 @@ def run_attacks(yaml_path, save_path, pdf, functions):
                 )
 
                 attack.run()
-                attack.create_attack_section(save_path)
+                attack.create_attack_section(report_save_path)
                 sections.append(attack.report_section)
 
-        report.report_generator(save_path, sections, pdf=pdf)
+                pickle_attack_obj(attack)
+
+        report.report_generator(report_save_path, sections, pdf=pdf)
+
+    return attack_object_paths
