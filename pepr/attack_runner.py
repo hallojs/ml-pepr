ff16d500e7f64eea78f4a38708faa86c8839a04a
Christopher Peredy <16451370+mastercaution@users.noreply.github.com>
2021-04-19 12:48:23 +0200

    Rework attack runner - make more generic

--- pepr/attack_runner.py
+++ pepr/attack_runner.py
@@ -18,23 +18,16 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def run_attacks(
-    yaml_path, report_save_path, attack_obj_save_path, functions, pdf=False
-):
+def run_attacks(yaml_path, attack_obj_save_path, functions):
     """
-    Run multtiple attacks configured in a YAML file.
+    Run multiple attacks configured in a YAML file.
 
     Parameters
     ----------
     yaml_path : str
         Path to attack configuration file in YAML format.
-    report_save_path : str
-        Path where to save the report files.
     attack_obj_save_path : str
         Path where to save the serialized attack objects.
-    pdf : bool
-        If true, the attack runner will build the LaTex report and save an PDF to the
-        save path.
     functions : dict
         Dictionary containing the functions which return TensorFlow models. The keys
         should correspond to the strings in the configuration file.
@@ -42,9 +35,28 @@ def run_attacks(
     Returns
     -------
     dict
-        Dictionary with attack alias and the corossponding path to the serialized attack
+        Dictionary with attack alias and the corresponding path to the serialized attack
         object.
     """
+
+    def load_function_pointer(key, f_key) -> dict:
+        return {key: functions[f_key]}
+
+    def load_numpy(key, path) -> dict:
+        return {key: np.load(path)}
+
+    # Prefix to function mapping. If a key starts with one of these prefixes, the prefix
+    # gets removed and the function will be called with the value.
+    prefix_function_map = {
+        "<fn>": load_function_pointer,
+        "<np>": load_numpy,
+    }
+
+    # Attack type to attack constructor mapping. Add new attacks here.
+    attack_constructor_map = {
+        "mia": mia.Mia,
+        "gmia": gmia.DirectGmia,
+    }
 
     attack_object_paths = {}
 
@@ -62,147 +74,67 @@ def run_attacks(
     # Parse YAML
     with open(yaml_path) as f_stream:
         logger.info(f"Parse attack runner configuration file: {yaml_path}")
-        data = yaml.load(f_stream)
-        attack_obj_save_path
+        yaml_data = yaml.load(f_stream, Loader=yaml.SafeLoader)
 
         os.makedirs(attack_obj_save_path, exist_ok=True)
-        os.makedirs(report_save_path + "/fig", exist_ok=True)
-        sections = []
 
-        for i, yaml_attack_pars in enumerate(data["attack_pars"]):
+        for i, yaml_attack_pars in enumerate(yaml_data["attack_pars"]):
+            yaml_data_conf = yaml_data["data_conf"][i]
+            yaml_attack_pars_rem = yaml_attack_pars.copy()
             attack_type = yaml_attack_pars["attack_type"]
             logger.info(
                 f"Attack number: {i+1}\n\n"
                 "######################## Attack Run ########################"
             )
-            if attack_type == "mia":
-                # Parse MIA configuration
-                logger.info("Setup configuration.")
-                shadow_model_function = functions[
-                    yaml_attack_pars["create_compile_shadow_model"]
-                ]
-                attack_model_function = functions[
-                    yaml_attack_pars["create_compile_attack_model"]
-                ]
 
-                yaml_data_conf = data["data_conf"][i]
-                shadow_indices = list(np.load(yaml_data_conf["shadow_indices_path"]))
-                target_indices = list(np.load(yaml_data_conf["target_indices_path"]))
-                evaluation_indices = list(
-                    np.load(yaml_data_conf["evaluation_indices_path"])
-                )
-                record_indices_per_target = np.load(
-                    yaml_data_conf["record_indices_per_target_path"]
-                )
+            attack_pars = {}
+            data_conf = {}
 
-                target_models = []
-                for path in yaml_attack_pars["target_model_paths"]:
-                    target_models.append(tf.keras.models.load_model(path))
+            # Dictionary parsing
+            logger.info("Setup configuration.")
+            for k, func in prefix_function_map.items():
+                for key, value in yaml_attack_pars.items():
+                    if key.startswith(k):
+                        real_key = key.replace(k, "", 1)
+                        attack_pars.update(func(real_key, value))
+                        del yaml_attack_pars_rem[key]
+                for key, value in yaml_data_conf.items():
+                    if key.startswith(k):
+                        real_key = key.replace(k, "", 1)
+                        data_conf.update(func(real_key, value))
 
-                # Setup dictionaries for attack
-                attack_pars = {
-                    "number_classes": yaml_attack_pars["number_classes"],
-                    "number_shadow_models": yaml_attack_pars["number_shadow_models"],
-                    "shadow_training_set_size": yaml_attack_pars[
-                        "shadow_training_set_size"
-                    ],
-                    "create_compile_shadow_model": shadow_model_function,
-                    "create_compile_attack_model": attack_model_function,
-                    "shadow_epochs": yaml_attack_pars["shadow_epochs"],
-                    "shadow_batch_size": yaml_attack_pars["shadow_batch_size"],
-                    "attack_epochs": yaml_attack_pars["attack_epochs"],
-                    "attack_batch_size": yaml_attack_pars["attack_batch_size"],
-                }
+            target_models = []
+            for path in yaml_attack_pars["target_model_paths"]:
+                target_models.append(tf.keras.models.load_model(path))
+            del yaml_attack_pars_rem["target_model_paths"]
 
-                data_conf = {
-                    "shadow_indices": shadow_indices,
-                    "target_indices": target_indices,
-                    "evaluation_indices": evaluation_indices,
-                    "record_indices_per_target": record_indices_per_target,
-                }
+            data = np.load(yaml_attack_pars["path_to_dataset_data"])
+            labels = np.load(yaml_attack_pars["path_to_dataset_labels"])
+            del yaml_attack_pars_rem["path_to_dataset_data"]
+            del yaml_attack_pars_rem["path_to_dataset_labels"]
 
-                # Create attack object
-                logger.info(f"Attack: {yaml_attack_pars['attack_alias']}")
-                attack = mia.Mia(
-                    yaml_attack_pars["attack_alias"],
-                    attack_pars,
-                    np.load(yaml_attack_pars["path_to_dataset_data"]),
-                    np.load(yaml_attack_pars["path_to_dataset_labels"]),
-                    data_conf,
-                    target_models,
-                )
+            attack_alias = yaml_attack_pars["attack_alias"]
+            del yaml_attack_pars_rem["attack_alias"]
 
-                attack.run()
-                attack.create_attack_section(report_save_path)
-                sections.append(attack.report_section)
+            # Add remaining plaintext parameters
+            del yaml_attack_pars_rem["attack_type"]
+            attack_pars.update(yaml_attack_pars_rem)
 
-                logger.debug("Serialize attack object.")
-                pickle_attack_obj(attack)
+            logger.info(f"Attack alias: {yaml_attack_pars['attack_alias']}")
+            logger.debug(f"attack_pars = {attack_pars}")
+            logger.debug(f"data_conf = {data_conf}")
+            attack = attack_constructor_map[attack_type](
+                attack_alias,
+                attack_pars,
+                data,
+                labels,
+                data_conf,
+                target_models,
+            )
 
-            elif attack_type == "gmia":
-                logger.info("Setup configuration.")
-                # Parse MIA configuration
-                model_function = functions[yaml_attack_pars["create_compile_model"]]
-
-                yaml_data_conf = data["data_conf"][i]
-                reference_indices = list(
-                    np.load(yaml_data_conf["reference_indices_path"])
-                )
-                target_indices = list(np.load(yaml_data_conf["target_indices_path"]))
-                evaluation_indices = list(
-                    np.load(yaml_data_conf["evaluation_indices_path"])
-                )
-                record_indices_per_target = np.load(
-                    yaml_data_conf["record_indices_per_target_path"]
-                )
-
-                target_models = []
-                for path in yaml_attack_pars["target_model_paths"]:
-                    target_models.append(tf.keras.models.load_model(path))
-
-                # Setup dictionaries for attack
-                attack_pars = {
-                    "number_classes": yaml_attack_pars["number_classes"],
-                    "number_reference_models": yaml_attack_pars[
-                        "number_reference_models"
-                    ],
-                    "reference_training_set_size": yaml_attack_pars[
-                        "reference_training_set_size"
-                    ],
-                    "create_compile_model": model_function,
-                    "reference_epochs": yaml_attack_pars["reference_epochs"],
-                    "reference_batch_size": yaml_attack_pars["reference_batch_size"],
-                    "hlf_metric": yaml_attack_pars["hlf_metric"],
-                    "hlf_layer_number": yaml_attack_pars["hlf_layer_number"],
-                    "number_target_records": yaml_attack_pars["number_target_records"],
-                }
-
-                data_conf = {
-                    "reference_indices": reference_indices,
-                    "target_indices": target_indices,
-                    "evaluation_indices": evaluation_indices,
-                    "record_indices_per_target": record_indices_per_target,
-                }
-
-                # Create attack object
-                logger.info(f"Attack: {yaml_attack_pars['attack_alias']}")
-                attack = gmia.DirectGmia(
-                    yaml_attack_pars["attack_alias"],
-                    attack_pars,
-                    np.load(yaml_attack_pars["path_to_dataset_data"]),
-                    np.load(yaml_attack_pars["path_to_dataset_labels"]),
-                    data_conf,
-                    target_models,
-                )
-
-                attack.run()
-                attack.create_attack_section(report_save_path)
-                sections.append(attack.report_section)
-
-                logger.debug("Serialize attack object.")
-                pickle_attack_obj(attack)
-
-        logger.info("Generate final report.")
-        report.report_generator(report_save_path, sections, pdf=pdf)
+            logger.info("Running attack.")
+            attack.run()
+            logger.debug("Serialize attack object.")
+            pickle_attack_obj(attack)
 
     return attack_object_paths
